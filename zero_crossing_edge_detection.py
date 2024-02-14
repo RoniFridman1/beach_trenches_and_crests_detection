628bd383bd694a9252cd441a0e129f1e31fd1912
RoniFridman1 <110767276+RoniFridman1@users.noreply.github.com>
2024-02-14 10:16:19 +0200

    Add files via upload
    init

---
+++ zero_crossing_edge_detection.py
@@ -0,0 +1,84 @@
+#Implementation of Canny Edge Detector
+import os
+import time
+
+from utils import *
+from canny_utils import *
+
+input_path = "./test_data/"
+output_path = "./canny_output/"
+img_list = [path for path in os.listdir(input_path)]
+# img_list = [path for path in os.listdir(input_path) if ".xyz" in path]
+show_images = False
+save_images = True
+
+kernel_type = 'dog'
+
+# Load the input images
+for img in img_list:
+    extension = img.split(".")[-1]
+    is_input_color = extension in ["png","jpeg","jpg"]
+    if not is_input_color and not extension == 'xyz':
+        print("Illegal format. Skipping file")
+        continue
+
+    img_output_path = output_path + img.split('.')[0]
+    if not is_input_color:
+        xyzfilename = input_path + img
+        input_img, delimiter, resolution_in_meters, min_x, min_y, min_z, df= convert_xyz_to_img(input_path + img)
+    else:
+        input_img = imageio.v2.imread(input_path + img)
+    if show_images:
+        plt.imshow(input_img)
+        plt.show()
+    kernel = DoG_kernel if kernel_type.lower() == 'dog' else LoG_kernel # Can use DoG_kernel or LoG_kernel
+    diff_of_gauss_img = convolve2d(input_img, kernel)
+
+    zero_crossing_dog = zero_cross_detection(diff_of_gauss_img)
+    # zero_crossing_dog = handle_img_padding(input_img, zero_crossing_dog)
+    if show_images:
+        plot_input(zero_crossing_dog, 'DoG - Zero Crossing')
+    ksize = 3
+    delta = 2
+    sobelx = cv2.Sobel(diff_of_gauss_img, cv2.CV_64F, delta, 0, ksize=ksize)
+    sobely = cv2.Sobel(diff_of_gauss_img, cv2.CV_64F, 0, delta, ksize=ksize)
+    sobel_first_derivative = cv2.magnitude(sobelx, sobely)
+    # plot_input(sobel_first_derivative, 'Sobel First Order Derivative')
+    if show_images:
+        plt.hist(sobel_first_derivative)
+        plt.show()
+
+    sobel_test = np.empty_like(sobel_first_derivative)
+    sobel_test[:] = sobel_first_derivative / 255
+    sobel_test = sobel_test * 255 / np.max(sobel_test)
+
+    # if show_images:
+    plt.hist(sobel_test)
+    plt.show()
+
+    threshold = 0.9
+    sobel_test[sobel_test > threshold] = 255
+    sobel_test[sobel_test < threshold] = 0
+    output_img = cv2.bitwise_and(zero_crossing_dog, sobel_test)
+
+
+    output_filename = f"output_ksize-{ksize}_thrsh-{threshold}_krnl-{kernel_type}_delta-{delta}"
+    print(output_filename)
+    if show_images:
+        plot_input(sobel_test, 'Boosted 1st order Derivative - Sobel edges')
+        plot_input(output_img, 'Strong Edges detected by DoG Zero Crossing')
+
+    if save_images:
+        plt.imsave(img_output_path + f"/{output_filename}.png", output_img, cmap=plt.get_cmap('gray'))
+
+    # Phase 4 : If origin is XYZ, convert to XYZ
+    t0 = time.time()
+    if not is_input_color:
+        convert_img_to_coordinates(output_img, df, img_output_path + f"/{output_filename}.xyz",
+                                   delimiter, resolution_in_meters, min_x, min_y, min_z,
+                                   out_format='xyz')
+        # for shp output
+        # convert_img_to_coordinates(output_img, df, img_output_path + f"/{output_filename}.xyz",
+        #                            delimiter, resolution_in_meters, min_x, min_y, min_z,
+        #                            out_format='shp')
+    print(f"Finished Phase 4 in {round(time.time() - t0)} seconds")
